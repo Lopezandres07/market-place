a8f01f44e62be86fe5a71f79da2a83105f550373
Cristian Arevalo <crsarevalo@gmail.com>
2024-02-28 11:38:00 -0300

    adding favorites and setting the card format

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Products from "./Views/Products";
+import Favorites from "./Views/Favorites";
 
 function App() {
   return (
