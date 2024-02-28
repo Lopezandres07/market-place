bee0c03c1b6df3358c2381bd07f5b0c2c044b102
Andrés López <andlop17@gmail.com>
2024-02-28 10:30:42 -0300

    Adding Routes in App.jsx and Main.jsx

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,15 +1,67 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Products from "./Views/Products";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Products from './Views/Products'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
+  const { token } = useContext(UserContext)
+
   return (
-    <>
-      <div className="App">
-        <Products />
-      </div>
-    </>
-  );
+    <main>
+      <Routes>
+        <Route
+          path='/'
+          element={<HomePage />}
+        />
+        <Route
+          path='/login'
+          element={<LoginPage />}
+        />
+        <Route
+          path='/register'
+          element={<RegisterPage />}
+        />
+        <Route
+          path='/Products'
+          element={<Products />}
+        />
+        <Route
+          path='*'
+          element={<NotFound />}
+        />
+
+        {/* Admin Routes */}
+
+        <Route
+          path='/admin/profile'
+          element={token ? <AdminProfile /> : <Navigate to='/login' />}
+        />
+        <Route
+          path='/admin/publications'
+          element={token ? <AdminPublications /> : <Navigate to='/login' />}
+        />
+        <Route
+          path='/admin/publications/create'
+          element={token ? <CreatePublication /> : <Navigate to='/login' />}
+        />
+
+        {/* User Routes */}
+
+        <Route
+          path='/user/profile'
+          element={token ? <UserProfile /> : <Navigate to='/login' />}
+        />
+        <Route
+          path='/user/products'
+          element={token ? <Products /> : <Navigate to='/login' />}
+        />
+        <Route
+          path='/user/favorites'
+          element={token ? <Favorites /> : <Navigate to='/login' />}
+        />
+      </Routes>
+    </main>
+  )
 }
 
-export default App;
+export default App
--- frontEnd/src/Views/Products.jsx
+++ frontEnd/src/Views/Products.jsx
@@ -1,91 +1,95 @@
-import React, { useState } from "react";
-import { Card, Row, Col, Button, Modal } from "react-bootstrap";
-import "../App.css";
+import React, { useState } from 'react'
+import { Card, Row, Col, Button, Modal } from 'react-bootstrap'
+import '../App.css'
 
 const Products = () => {
-  const [showDetails, setShowDetails] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [showDetails, setShowDetails] = useState(false)
+  const [selectedProduct, setSelectedProduct] = useState(null)
 
   const handleDetailsClick = (product) => {
-    setSelectedProduct(product);
-    setShowDetails(true);
-  };
+    setSelectedProduct(product)
+    setShowDetails(true)
+  }
 
   const handleCloseDetails = () => {
-    setShowDetails(false);
-  };
+    setShowDetails(false)
+  }
 
   const products = [
     {
       id: 1,
-      name: "smartphone",
+      name: 'smartphone',
       description:
-        "smartphone de vanguardia Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?",
+        'smartphone de vanguardia Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?',
       price: 2000000,
       urlimage:
-        "https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        'https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
       id: 2,
-      name: "BMW",
+      name: 'BMW',
       description:
-        "auto de vanguardia Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?",
+        'auto de vanguardia Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?',
       price: 2000000,
       urlimage:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        'https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
       id: 3,
-      name: "NES",
+      name: 'NES',
       description:
-        "comsola retro Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?",
+        'comsola retro Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?',
       price: 2000000,
       urlimage:
-        "https://images.pexels.com/photos/9100862/pexels-photo-9100862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        'https://images.pexels.com/photos/9100862/pexels-photo-9100862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
       id: 4,
-      name: "NES",
+      name: 'NES',
       description:
-        "comsola retro Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?",
+        'comsola retro Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?',
       price: 2000000,
       urlimage:
-        "https://images.pexels.com/photos/9100862/pexels-photo-9100862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        'https://images.pexels.com/photos/9100862/pexels-photo-9100862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
       id: 5,
-      name: "NES",
+      name: 'NES',
       description:
-        "comsola retro Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?",
+        'comsola retro Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad amet aspernatur iusto quam sit accusamus error eum expedita autem dicta facere cumque doloribus, laboriosam repellat quia nulla, asperiores odit?',
       price: 2000000,
       urlimage:
-        "https://images.pexels.com/photos/9100862/pexels-photo-9100862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        'https://images.pexels.com/photos/9100862/pexels-photo-9100862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
-  ];
+  ]
 
   return (
-    <>
-      <Row xs={1} sm={2} md={4}>
+    <div>
+      <Row
+        xs={1}
+        sm={2}
+        md={4}
+      >
         {products.map((item) => (
           <Col key={item.id}>
-            <div className="homeCard">
+            <div className='homeCard'>
               <Card>
                 <Card.Img
-                  variant="top"
+                  variant='top'
                   src={item.urlimage}
-                  className="product-image"
+                  className='product-image'
                 />
                 <Card.Body>
                   <Card.Title>{item.name}</Card.Title>
 
                   <Card.Text>
-                    Descripción: <br />{" "}
+                    Descripción: <br />{' '}
                     {item.description.length > 20
-                      ? item.description.substring(0, 20) + "..."
-                      : item.description}{" "}
+                      ? item.description.substring(0, 20) + '...'
+                      : item.description}{' '}
                   </Card.Text>
                   <Card.Text> Precio: $ {item.price}</Card.Text>
-                  <div className="d-flex justify-content-center">
+                  <div className='d-flex justify-content-center'>
                     <Button
                       key={item.id}
                       onClick={() => handleDetailsClick(item)}
@@ -100,7 +104,10 @@ const Products = () => {
         ))}
       </Row>
       {/* vista superpuesta */}
-      <Modal show={showDetails} onHide={handleCloseDetails}>
+      <Modal
+        show={showDetails}
+        onHide={handleCloseDetails}
+      >
         <Modal.Header closeButton>
           <Modal.Title>{selectedProduct && selectedProduct.name}</Modal.Title>
         </Modal.Header>
@@ -108,14 +115,14 @@ const Products = () => {
           <img
             src={selectedProduct && selectedProduct.urlimage}
             alt={selectedProduct && selectedProduct.name}
-            className="modal-image"
+            className='modal-image'
           />
           <p>{selectedProduct && selectedProduct.description}</p>
           <p>Precio: $ {selectedProduct && selectedProduct.price}</p>
         </Modal.Body>
       </Modal>
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default Products;
+export default Products
