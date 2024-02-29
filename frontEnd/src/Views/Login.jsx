c4ea894d02f6dd7b31507f6a863b351324961d8a
Andrés López <andlop17@gmail.com>
2024-02-29 01:08:18 -0300

    Working on login view part 2

--- frontEnd/src/Views/Login.jsx
+++ frontEnd/src/Views/Login.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 const Login = () => {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  console.log(errors)
+
   const onSubmit = handleSubmit((data) => {
     console.log(data)
   })
@@ -12,37 +19,66 @@ const Login = () => {
       <label htmlFor='Nombre'>Nombre</label>
       <input
         type='text'
-        {...register('Nombre')}
+        {...register('Nombre', {
+          required: {
+            value: true,
+            message: 'Nombre es requerido',
+          },
+          minLength: {
+            value: 2,
+            message: 'Nombre debe tener al menos 2 caracteres',
+          },
+        })}
       />
+      {errors.Nombre && <span>{errors.Nombre.message}</span>}
 
       <label htmlFor='Apellido'>Apellido</label>
       <input
         type='text'
-        {...register('Apellido')}
+        {...register('Apellido', {
+          required: true,
+        })}
       />
 
       <label htmlFor='Email'>Correo</label>
       <input
         type='email'
-        {...register('Email')}
+        {...register('Email', {
+          required: {
+            value: true,
+            message: 'Correo es requerido',
+          },
+          pattern: {
+            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/,
+            message: 'Correo no valido',
+          },
+        })}
       />
+
+      {errors.Email && <span>{errors.Email.message}</span>}
 
       <label htmlFor='Password'>contraseña</label>
       <input
         type='password'
-        {...register('Contraseña')}
+        {...register('Contraseña', {
+          required: true,
+        })}
       />
 
       <label htmlFor='Password'>Confirmar contraseña</label>
       <input
         type='password'
-        {...register('Password')}
+        {...register('Password', {
+          required: true,
+        })}
       />
 
       <label htmlFor='Foto'>Foto de perfil</label>
       <input
         type='file'
-        {...register('Foto')}
+        {...register('Foto', {
+          required: true,
+        })}
       />
 
       <button>Enviar</button>
