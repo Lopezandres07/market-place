7083e737386ec65caae3ec266906a40258cc5655
Andrés López <andlop17@gmail.com>
2024-03-02 20:32:51 -0300

    Adding view to create products

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,43 +1,59 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route, Navigate } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
-import { UserContext } from "./providers/UserProvider";
-import { useContext, useState } from "react";
-import NotFound from "./Views/NotFound";
-import Home from "./Views/Home";
-import Register from "./Views/Register";
-import Login from "./Views/Login";
-import HomeUser from "./Views/HomeUser";
-import FavoritesUser from "./Views/FavoritesUser";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import NavigationBar from './components/NavigationBar'
+import { UserContext } from './providers/UserProvider'
+import { useContext, useState } from 'react'
+import NotFound from './Views/NotFound'
+import Home from './Views/Home'
+import Register from './Views/Register'
+import Login from './Views/Login'
+import HomeUser from './Views/HomeUser'
+import FavoritesUser from './Views/FavoritesUser'
+import CreatePublication from './Views/CreatePublication'
 
 function App() {
-  const { token } = useContext(UserContext);
+  const { token } = useContext(UserContext)
 
   // Estado para almacenar la lista de favoritos
-  const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([])
 
   // Función para agregar un producto a la lista de favoritos
   const addToFavorites = (product) => {
-    setFavorites([...favorites, product]);
-  };
+    setFavorites([...favorites, product])
+  }
 
   // Función para eliminar un producto de la lista de favoritos
   const removeFromFavorites = (product) => {
-    setFavorites(favorites.filter((fav) => fav.id !== product.id));
-  };
+    setFavorites(favorites.filter((fav) => fav.id !== product.id))
+  }
 
   return (
     <main>
       <NavigationBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/homeUser" element={<HomeUser />} />
-        <Route path="*" element={<NotFound />} />
         <Route
-          path="/favoritesUser"
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/register'
+          element={<Register />}
+        />
+        <Route
+          path='/homeUser'
+          element={<HomeUser />}
+        />
+        <Route
+          path='*'
+          element={<NotFound />}
+        />
+        <Route
+          path='/favoritesUser'
           element={
             <FavoritesUser
               favorites={favorites}
@@ -45,7 +61,10 @@ function App() {
             />
           }
         />
-
+        <Route
+          path='/admin/publications/create'
+          element={<CreatePublication />}
+        />
         {/* Admin Routes */}
 
         {/*  <Route
@@ -81,7 +100,7 @@ function App() {
         />*/}
       </Routes>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontEnd/src/Views/Register.jsx
+++ frontEnd/src/Views/Register.jsx
@@ -37,99 +37,104 @@ const Register = () => {
   })
 
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor='firstName'>Nombre</label>
-      <input
-        type='text'
-        {...register('firstName', {
-          required: {
-            value: true,
-            message: 'Nombre es requerido',
-          },
-          minLength: {
-            value: 2,
-            message: 'Nombre debe tener al menos 2 caracteres',
-          },
-        })}
-      />
-      {errors.fistName && <span>{errors.fistName.message}</span>}
+    <>
+      <h1>Creación de usuario</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor='firstName'>Nombre</label>
+        <input
+          type='text'
+          {...register('firstName', {
+            required: {
+              value: true,
+              message: 'Nombre es requerido',
+            },
+            minLength: {
+              value: 2,
+              message: 'Nombre debe tener al menos 2 caracteres',
+            },
+          })}
+        />
+        {errors.fistName && <span>{errors.fistName.message}</span>}
 
-      <label htmlFor='lastName'>Apellido</label>
-      <input
-        type='text'
-        {...register('lastName', {
-          required: {
-            value: true,
-            message: 'Apellido es requerido',
-          },
-          minLength: {
-            value: 2,
-            message: 'Apellido debe tener al menos 2 caracteres',
-          },
-        })}
-      />
+        <label htmlFor='lastName'>Apellido</label>
+        <input
+          type='text'
+          {...register('lastName', {
+            required: {
+              value: true,
+              message: 'Apellido es requerido',
+            },
+            minLength: {
+              value: 2,
+              message: 'Apellido debe tener al menos 2 caracteres',
+            },
+          })}
+        />
 
-      {errors.lastName && <span>{errors.lastName.message}</span>}
+        {errors.lastName && <span>{errors.lastName.message}</span>}
 
-      <label htmlFor='email'>Correo</label>
-      <input
-        type='email'
-        {...register('email', {
-          required: {
-            value: true,
-            message: 'Correo es requerido',
-          },
-          pattern: {
-            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/,
-            message: 'Correo no valido',
-          },
-        })}
-      />
+        <label htmlFor='email'>Correo</label>
+        <input
+          type='email'
+          {...register('email', {
+            required: {
+              value: true,
+              message: 'Correo es requerido',
+            },
+            pattern: {
+              value: /[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/,
+              message: 'Correo no valido',
+            },
+          })}
+        />
 
-      {errors.email && <span>{errors.email.message}</span>}
+        {errors.email && <span>{errors.email.message}</span>}
 
-      <label htmlFor='password'>Contraseña</label>
-      <input
-        type='password'
-        {...register('password', {
-          required: {
-            value: true,
-            message: 'Contraseña es requerida',
-          },
-          minLength: {
-            value: 6,
-            message: 'Contraseña debe tener al menos 6 caracteres',
-          },
-        })}
-      />
+        <label htmlFor='password'>Contraseña</label>
+        <input
+          type='password'
+          {...register('password', {
+            required: {
+              value: true,
+              message: 'Contraseña es requerida',
+            },
+            minLength: {
+              value: 6,
+              message: 'Contraseña debe tener al menos 6 caracteres',
+            },
+          })}
+        />
 
-      {errors.password && <span>{errors.password.message}</span>}
+        {errors.password && <span>{errors.password.message}</span>}
 
-      <label htmlFor='password'>Confirmar contraseña</label>
-      <input
-        type='password'
-        {...register('passwordConfirm', {
-          required: {
-            value: true,
-            message: 'Contraseña es requerida',
-          },
-          validate: (value) =>
-            value === watch('password') || 'Las contraseñas no coinciden',
-        })}
-      />
+        <label htmlFor='password'>Confirmar contraseña</label>
+        <input
+          type='password'
+          {...register('passwordConfirm', {
+            required: {
+              value: true,
+              message: 'Contraseña es requerida',
+            },
+            validate: (value) =>
+              value === watch('password') || 'Las contraseñas no coinciden',
+          })}
+        />
 
-      {errors.passwordConfirm && <span>{errors.passwordConfirm.message}</span>}
+        {errors.passwordConfirm && (
+          <span>{errors.passwordConfirm.message}</span>
+        )}
 
-      <label htmlFor='Foto'>Foto de perfil</label>
-      <input
-        type='file'
-        onChange={(e) => {
-          setValue('avatarURL', e.target.files[0].name)
-        }}
-      />
+        <label htmlFor='photo'>Foto de perfil</label>
+        <input
+          type='file'
+          onChange={(e) => {
+            setValue('avatarURL', e.target.files[0].name)
+          }}
+        />
 
-      <button className='mt-2'>Enviar</button>
-    </form>
+        <button className='mt-2'>Crear usuario</button>
+      </form>
+    </>
   )
 }
 
