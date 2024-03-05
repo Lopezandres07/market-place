fd7a250f01db440d18a9dfb771d9cb59409ca310
Andrés López <andlop17@gmail.com>
2024-03-05 20:18:12 -0300

    Adding Squema and some folders

---
+++ backEnd/config/db/squema.sql
@@ -0,0 +1,42 @@
+// Creacion de la base de datos
+
+CREATE DATABASE columpios;
+
+\c columpios
+
+// Creacion de las tablas
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    role_id INT REFERENCES roles(id),
+    firstName VARCHAR (50) NOT NULL,
+    lastName VARCHAR (50) NOT NULL,
+    avatarURL VARCHAR (50) NOT NULL,
+    email VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR(150) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()   
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    price INT NOT NULL,
+    imageURL VARCHAR (50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()   
+);
+
+CREATE TABLE users_favourites (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    product_id INT REFERENCES products(id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL
+)
