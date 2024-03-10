1f509c5e8b7fae5f201968ca89831e87af984a86
Andrés López <andlop17@gmail.com>
2024-03-10 18:14:55 -0300

    Adding middleware to login route

--- backEnd/config/routes/userRoutes.js
+++ backEnd/config/routes/userRoutes.js
@@ -5,11 +5,12 @@ import {
   loginUser,
 } from '../../src/api/v1/Controllers/userControllers.js'
 import { validateParametersUser } from '../../middlewares/validateParamsUser.js'
+import { validparameters } from '../../middlewares/validateParametersLogin.js'
 
 const router = express.Router()
 
 router.post('/register', validateParametersUser, createNewUser)
-router.post('/login', /* middleware */ loginUser)
+router.post('/login', validparameters, loginUser)
 router.post('/googleLogin', googleLogin)
 
 export default router
