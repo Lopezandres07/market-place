2473ab16a49e8608b8454ee6306e4734fd5bb54f
Andrés López <andlop17@gmail.com>
2024-03-13 19:52:34 -0300

    Adjusting Server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,25 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import swagger from './config/swagger/swagger.js';
-import userRoutes from './config/routes/userRoutes.js';
-import productsRoutes from './config/routes/productsRoutes.js';
+import express from 'express'
+import cors from 'cors'
+import swagger from './config/swagger/swagger.js'
+import userRoutes from './config/routes/userRoutes.js'
+import productsRoutes from './config/routes/productsRoutes.js'
 
-const app = express();
-app.use('*', cors());
+const app = express()
+app.use('*', cors())
 
-const PORT = 3000;
-swagger(app);
+const PORT = 3000
+swagger(app)
 
-app.use(express.json());
-app.use('/api/v1', userRoutes);
-app.use('/api/v1', productsRoutes);
+app.use(express.json())
+app.use('/api/v1', userRoutes)
+app.use('/api/v1', productsRoutes)
 
-// Verificar si este archivo es el punto de entrada principal
+/* // Verificar si este archivo es el punto de entrada principal
 if (require.main === module) {
   app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
     console.log(`Swagger docs available at http://localhost:${PORT}/api/v1/docs`);
   });
-}
+} */
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Swagger docs available at http://localhost:${PORT}/api/v1/docs`)
+})
+
+export default app
