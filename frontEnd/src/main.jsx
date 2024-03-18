037b44e6a225b231bc9c88d678f45276b8f6d82e
Cristian Arevalo <crsarevalo@gmail.com>
2024-03-18 01:46:28 -0300

    updating some styles, and getting fixed the user_id creating products

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import UserProvider from './providers/UserProvider.jsx'
-import { FavoritesProvider } from './providers/FavoritesContext.jsx'
-import { ProductsProvider } from './providers/productsContext.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import UserProvider from "./providers/UserProvider.jsx";
+import { FavoritesProvider } from "./providers/FavoritesContext.jsx";
+import { ProductsProvider } from "./providers/productsContext.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-        <ProductsProvider>
-          <FavoritesProvider>
-            <UserProvider>
+        <UserProvider>
+          <ProductsProvider>
+            <FavoritesProvider>
               <App />
-            </UserProvider>
-          </FavoritesProvider>
-        </ProductsProvider>
+            </FavoritesProvider>
+          </ProductsProvider>
+        </UserProvider>
       </GoogleOAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
