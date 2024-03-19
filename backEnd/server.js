26283e780b8e12df13ee6afbef9cf4459d40feae
Cristian Arevalo <crsarevalo@gmail.com>
2024-03-19 00:12:59 -0300

    triying new route

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,31 +1,33 @@
-import express from 'express'
-import cors from 'cors'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-import swagger from './config/swagger/swagger.js'
-import userRoutes from './config/routes/userRoutes.js'
-import productsRoutes from './config/routes/productsRoutes.js'
+import express from "express";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import swagger from "./config/swagger/swagger.js";
+import userRoutes from "./config/routes/userRoutes.js";
+import productsRoutes from "./config/routes/productsRoutes.js";
 
-const app = express()
+const app = express();
 
 // Comentar para ejecutar test ⬇️
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-  console.log(`Swagger docs available at http://localhost:${PORT}/api/v1/docs`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(
+    `Swagger docs available at https://market-place-4d96.onrender.com/api/v1/docs`
+  );
+});
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-app.use('/uploads', express.static(join(__dirname, 'uploads')))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use("/uploads", express.static(join(__dirname, "uploads")));
 
 // Comentar para ejecutar test ⬆️
 
-app.use('*', cors())
-app.use(express.json())
-app.use('/api/v1', userRoutes)
-app.use('/api/v1', productsRoutes)
-swagger(app)
+app.use("*", cors());
+app.use(express.json());
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", productsRoutes);
+swagger(app);
 
-export default app
+export default app;
