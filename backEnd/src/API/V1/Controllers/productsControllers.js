21029aefa83d74e065e061761fe491684ef8c6c7
Andrés López <andlop17@gmail.com>
2024-03-19 01:20:41 -0300

    Adding favorites DB and creating HTTP request, controllers and models of it

--- backEnd/src/API/V1/Controllers/productsControllers.js
+++ backEnd/src/API/V1/Controllers/productsControllers.js
@@ -5,72 +5,84 @@ import {
   getAllProducts,
   removeFavorite,
   updateProduct,
-} from "../../../API/V1/Models/productsModels.js";
+} from '../../../API/V1/Models/productsModels.js'
 
 const getAllProductsController = async (req, res) => {
   try {
-    const products = await getAllProducts();
-    res.status(200).json(products);
+    const products = await getAllProducts()
+    res.status(200).json(products)
   } catch (error) {
-    console.error("Error en el controlador", error);
-    res.status(500).send("Error interno del servidor");
+    console.error('Error en el controlador', error)
+    res.status(500).send('Error interno del servidor')
   }
-};
+}
 
 const createNewProduct = async (req, res) => {
   try {
-    const { product } = req.body;
-    const adminRoleId = 1;
-    product.role_id = adminRoleId;
+    const { product } = req.body
+    const adminRoleId = 1
+    product.role_id = adminRoleId
 
-    const newProduct = await createProduct(product);
-    res.status(201).json({ product: newProduct });
+    const newProduct = await createProduct(product)
+    res.status(201).json({ product: newProduct })
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(400).json(error.message)
   }
-};
+}
 
 const removeProduct = async (req, res) => {
   try {
-    const { productId } = req.params;
-    console.log("ID recibido en el servidor:", productId);
-    const deletedProduct = await deleteProduct(productId);
+    const { productId } = req.params
+    console.log('ID recibido en el servidor:', productId)
+    const deletedProduct = await deleteProduct(productId)
     res.status(200).json({
-      message: "Producto eliminado exitosamente",
+      message: 'Producto eliminado exitosamente',
       product: deletedProduct,
-    });
+    })
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(400).json(error.message)
   }
-};
+}
 
 const updateProductController = async (req, res) => {
   try {
-    const { productId } = req.params;
-    const { name, description, price, imageURL } = req.body;
+    const { productId } = req.params
+    const { name, description, price, imageURL } = req.body
 
     const updatedProduct = await updateProduct(productId, {
       name,
       description,
       price,
       imageURL,
-    });
-    console.log(updateProduct);
+    })
+    console.log(updateProduct)
 
     res.status(200).json({
-      message: "Producto actualizado exitosamente",
+      message: 'Producto actualizado exitosamente',
       product: updatedProduct,
-    });
+    })
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(400).json(error.message)
+  }
+}
+
+const getFavoritesWithDetails = async (req, res) => {
+  const { id } = req.params
+  console.log(id)
+
+  try {
+    const favorites = await getFavoritesWithProductDetails(id)
+    res.status(200).json({ favorites })
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching favorites with details' })
   }
 }
 
 const addToFavorites = async (req, res) => {
-  const { product } = req.body
+  const { productId, userId } = req.body
 
   try {
-    const newFavorite = await addFavorite(product)
+    const newFavorite = await addFavorite(productId, userId)
     res.status(200).json({ success: true, newFavorite })
   } catch (error) {
     console.error('Error adding product to favorites:', error)
@@ -81,17 +93,14 @@ const addToFavorites = async (req, res) => {
 }
 
 const removeFromFavorites = async (req, res) => {
-  const { userId, productId } = req.body
+  const { id } = req.params
 
   try {
-    const removedFavorite = await removeFavorite(userId, productId)
-    res.status(200).json({ success: true, data: removedFavorite })
+    const removedFavorite = await removeFavorite(id)
+    res.status(200).json({ success: true, removedFavorite })
   } catch (error) {
-    console.error('Error removing product from favorites:', error)
-    res.status(500).json({
-      success: false,
-      message: 'Error removing product from favorites',
-    })
+    console.error('Error removing favorite:', error)
+    res.status(500).json({ success: false, message: 'Error removing favorite' })
   }
 }
 
@@ -100,7 +109,7 @@ export {
   removeProduct,
   getAllProductsController,
   updateProductController,
+  getFavoritesWithDetails,
   addToFavorites,
   removeFromFavorites,
 }
-
--- frontEnd/src/components/CardProducts.jsx
+++ frontEnd/src/components/CardProducts.jsx
@@ -1,44 +1,52 @@
-import React from "react";
-import { Card, Button } from "react-bootstrap";
-import "./componentsStyle/CardStyle.css";
+import React from 'react'
+import { Card, Button } from 'react-bootstrap'
+import './componentsStyle/CardStyle.css'
 
 const CardProducts = ({
   product,
   handleDetailsClick,
-  toggleFavorite,
   isFavorite,
+  addToFavorites,
+  userData,
 }) => {
+  const handleAddToFavorites = () => {
+    addToFavorites(product, userData)
+  }
+
   return (
     <section>
-      <div className="homeCard">
-        <Card className="shadow">
+      <div className='homeCard'>
+        <Card className='shadow'>
           <Card.Img
-            variant="top"
+            variant='top'
             src={product.imageurl}
-            className="product-image"
+            className='product-image'
           />
           <Card.Body>
             <Card.Title>{product.name}</Card.Title>
             <Card.Text>
               Descripción: <br />
               {product.description.length > 20
-                ? product.description.substring(0, 20) + "..."
+                ? product.description.substring(0, 20) + '...'
                 : product.description}
             </Card.Text>
-            <Card.Text className="precio">Precio: $ {product.price}</Card.Text>{" "}
-            <div className="buttons">
+            <Card.Text className='precio'>Precio: $ {product.price}</Card.Text>{' '}
+            <div className='buttons'>
               <Button
-                id="btn-detalles"
-                onClick={() => handleDetailsClick(product)}
+                id='btn-detalles'
+                onClick={() => handleDetailsClick()}
               >
                 Detalles 👀
               </Button>
-              <Button id="btn-favorite" onClick={() => toggleFavorite(product)}>
+              <Button
+                id='btn-favorite'
+                onClick={handleAddToFavorites}
+              >
                 <i
                   className={
                     isFavorite(product)
-                      ? "fas fa-heart favorite-heart"
-                      : "far fa-heart"
+                      ? 'fas fa-heart favorite-heart'
+                      : 'far fa-heart'
                   }
                 ></i>
               </Button>
@@ -47,7 +55,7 @@ const CardProducts = ({
         </Card>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default CardProducts;
+export default CardProducts
