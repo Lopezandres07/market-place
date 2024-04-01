dfdd6d92c38c57d35dc53b629d1f1943b26ee224
Lopezandres07 <andlop17@gmail.com>
2024-04-01 17:31:01 -0300

    Adjusting Navigation Bar and body's bg color

--- frontEnd/src/components/NavigationBar.jsx
+++ frontEnd/src/components/NavigationBar.jsx
@@ -12,78 +12,67 @@ const NavigationBar = () => {
 
   return (
     <>
-      <Navbar
-        className='d-flex justify-content-between px-4'
-        style={{
-          borderBottom: '5px solid #eabf3fb1',
-        }}
-      >
-        <Navbar.Brand
-          onClick={() => navigate(token ? '/homeUser' : '/')}
-          className='d-flex align-items-center'
-        >
-          <img
-            alt='Logo'
-            style={{ width: '6rem' }}
-            src='/img/logo.png'
-            className='logo'
-          />
-          <NavbarText style={{ color: 'black' }}>
+      <Navbar className='navigation px-4 py-0 justify-content-between align-items-center'>
+        <Navbar.Brand onClick={() => navigate(token ? '/homeUser' : '/')}>
+          <NavbarText>
             <h4>Rústico Kids</h4>
           </NavbarText>
         </Navbar.Brand>
-        <Nav>
-          {token && userData ? (
-            <div className='link'>
-              <NavLink
-                className={setActiveClass}
-                to='/homeUser'
-              >
-                Inicio
-              </NavLink>
-              <NavLink
-                className={setActiveClass}
-                to='/aboutUs'
-              >
-                Conóceme
-              </NavLink>
-              <NavLink
-                className={setActiveClass}
-                to={`/user/${userData.id}`}
-              >
-                Mi Perfil
-              </NavLink>
-              <NavLink
-                className={setActiveClass}
-                onClick={logout}
-                to='/'
-              >
-                Cerrar Sesión
-              </NavLink>
-            </div>
-          ) : (
-            <div>
-              <NavLink
-                className={setActiveClass}
-                to='/aboutUs'
-              >
-                Conóceme
-              </NavLink>
-              <NavLink
-                to='/login'
-                className={setActiveClass}
-              >
-                Iniciar Sesión
-              </NavLink>
-              <NavLink
-                to='/register'
-                className={setActiveClass}
-              >
-                Registrarse
-              </NavLink>
-            </div>
-          )}
-        </Nav>
+        <img
+          alt='Logo'
+          src='/img/newLogo.png'
+          className='logo'
+        />
+        {token && userData ? (
+          <Nav className='link ml-auto'>
+            <NavLink
+              className={setActiveClass}
+              to='/homeUser'
+            >
+              Inicio
+            </NavLink>
+            <NavLink
+              className={setActiveClass}
+              to='/aboutUs'
+            >
+              Conóceme
+            </NavLink>
+            <NavLink
+              className={setActiveClass}
+              to={`/user/${userData.id}`}
+            >
+              Mi Perfil
+            </NavLink>
+            <NavLink
+              className={setActiveClass}
+              onClick={logout}
+              to='/'
+            >
+              Cerrar Sesión
+            </NavLink>
+          </Nav>
+        ) : (
+          <Nav className='link ml-auto'>
+            <NavLink
+              className={setActiveClass}
+              to='/aboutUs'
+            >
+              Conóceme
+            </NavLink>
+            <NavLink
+              to='/login'
+              className={setActiveClass}
+            >
+              Iniciar Sesión
+            </NavLink>
+            <NavLink
+              to='/register'
+              className={setActiveClass}
+            >
+              Registrarse
+            </NavLink>
+          </Nav>
+        )}
       </Navbar>
     </>
   )
